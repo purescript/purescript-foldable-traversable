a9612ad0288d949805232604427b5e74571716b5
Martin Bidlingmaier <martin.bidlingmaier@gmail.com>
2017-06-15 19:45:22 +0200

    Add missing semicolon in js

--- src/Data/FunctorWithIndex.js
+++ src/Data/FunctorWithIndex.js
@@ -2,7 +2,7 @@
 
 exports.imapArray = function (f) {
   return function (xs) {
-    var l = xs.length
+    var l = xs.length;
     var result = Array(l);
     for (var i = 0; i < l; i++) {
       result[i] = f(i)(xs[i]);
