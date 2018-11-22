d30146ef988bead9d1c54f796b4da63d1c4d3c0f
David Smith <shmish111+github@gmail.com>
2018-11-22 15:47:10 +0000

    Add some examples to intercalate documentation

--- src/Data/Foldable.purs
+++ src/Data/Foldable.purs
@@ -240,6 +240,16 @@ oneOfMap f = foldr (alt <<< f) empty
 
 -- | Fold a data structure, accumulating values in some `Monoid`,
 -- | combining adjacent elements using the specified separator.
+-- |
+-- | For example:
+-- |
+-- | ```purescript
+-- | > intercalate ", " ["Lorem", "ipsum", "dolor"]
+-- | = "Lorem, ipsum, dolor"
+-- |
+-- | > intercalate "*" ["a", "b", "c"]
+-- | = "a*b*c"
+-- | ```
 intercalate :: forall f m. Foldable f => Monoid m => m -> f m -> m
 intercalate sep xs = (foldl go { init: true, acc: mempty } xs).acc
   where
