ae194b1a0077a815cede289258e188923b8c9642
milesfrain <milesfrain@users.noreply.github.com>
2020-10-14 18:45:07 -0700

    Add examples for sequence and traverse (#115)
    * Add examples for sequence and traverse
    
    * Review feedback, unnecessary parens and logShow

--- src/Data/Traversable.purs
+++ src/Data/Traversable.purs
@@ -32,6 +32,26 @@ import Data.Traversable.Accum.Internal (StateL(..), StateR(..), stateL, stateR)
 -- | - `sequence` runs the actions _contained_ in a data structure,
 -- |   and accumulates the results.
 -- |
+-- | ```purescript
+-- | import Data.Traversable
+-- | import Data.Maybe
+-- | import Data.Int (fromNumber)
+-- |
+-- | sequence [Just 1, Just 2, Just 3] == Just [1,2,3]
+-- | sequence [Nothing, Just 2, Just 3] == Nothing
+-- |
+-- | traverse fromNumber [1.0, 2.0, 3.0] == Just [1,2,3]
+-- | traverse fromNumber [1.5, 2.0, 3.0] == Nothing
+-- |
+-- | traverse logShow [1,2,3]
+-- | -- prints:
+-- |    1
+-- |    2
+-- |    3
+-- |
+-- | traverse (\x -> [x, 0]) [1,2,3] == [[1,2,3],[1,2,0],[1,0,3],[1,0,0],[0,2,3],[0,2,0],[0,0,3],[0,0,0]]
+-- | ```
+-- |
 -- | The `traverse` and `sequence` functions should be compatible in the
 -- | following sense:
 -- |
